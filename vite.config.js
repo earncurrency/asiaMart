4ec904cc3b2c7611b414be6a00b5830b212d73b5
earncurrency <earncurrency@gmail.com>
2025-02-14 08:21:24 +0700

    vit.config

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     // vueDevTools(),
   ],
-  base: '/asiaMart/',
+  base: process.env.NODE_ENV === 'production'?'/asiaMart/':'',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
